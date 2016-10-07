26d7dd6bfd7f2235c093eef12bc8659db00ae20c
Ondrej Profant <ondrej.profant@gmail.com>
2016-10-07 18:11:05 +0200

    První várka oprav

---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+---
+---
+{% comment %}
+Concat all js file into one.
+Except common used libs. They are loaded via CDN.
+{% endcomment %}
+{% include js/main.js %}
+{% include js/twitter.js %}
+{% include js/facebook.js %}
