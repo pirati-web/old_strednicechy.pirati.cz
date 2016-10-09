6baacb1ce342615d0cdbe22fc4386e7751465975
Ondřej Profant <ondrej.profant@gmail.com>
2016-10-09 17:47:22 +0200

    Oprava odkazů po změně GH-pages

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,10 +1,9 @@
 $(document).foundation();
 $(document).ready(function(){
 
-  // hide #back-top first
+  /* hide #back-top first */
   $("#back-top").hide();
-
-  // fade in #back-top
+  /* fade in #back-top */
   $(function () {
     $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
@@ -13,8 +12,7 @@ $(document).ready(function(){
         $('#back-top').fadeOut();
       }
     });
-
-    // scroll body to 0px on click
+    /* scroll body to 0px on click */
     $('#back-top .fi-arrow-up').click(function () {
       $('body,html').animate({
         scrollTop: 0
